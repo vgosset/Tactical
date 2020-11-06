c56defa2bc6a9a60ddf1b0d046f9285b2eb3d436
vgosset <valentingosset96@gmail.com>
2020-11-06 17:57:33 +0100

    + map generator

--- README.md
+++
@@ -1,9 +0,0 @@
-# Tactical
-
-Note pour la canditature Motion Twin :
-
-La build standalone est dans le folder build. 
-
-J'ai arrêté de travailler sur ce prototype il y a 1.5 mois après avoir préparé des outils pour faire du Level Design, il n'y a donc pas encore de vrai LD et rien n'est vraiment balance correctement. 
-
-J'avais prévu de me remettre dessus à mon retour en France pour commencer à faire mon premier LD et mes premiers types de personnages avec un premier balancing, et également profiter du moment ou je voulais minitier à la 3D (vfx, modélisation, animation...) pour pouvoir utiliser mes premières assets dedans.
---
+++ Tactics/Assets/MapGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject tile;
+    [SerializeField] private GameObject obstacle;
+    [SerializeField] private Transform container;
+
+    [Space(10)]
+    [SerializeField] private int rows;
+    [SerializeField] private int columns;
+    
+    [Space(10)]
+    
+    [SerializeField] private int obstacle_a;
+    private List<Vector3> obsPos = new List<Vector3>();
+    private void Start()
+    {
+        CreateObstacles();
+        GenerateMap();
+        // StartCoroutine(GenerateMap()); 
+    }
+    private void CreateObstacles()
+    {
+        for (int i = 0; i < obstacle_a; i++)
+        {
+            Vector3 rdnPos = new Vector3(Random.Range(0, rows), 0, Random.Range(0, columns));
+            obsPos.Add(rdnPos);
+
+            GameObject tmp = Instantiate(obstacle, rdnPos, Quaternion.identity);
+            tmp.transform.parent = container;
+        }
+    }
+    private void GenerateMap()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Vector3 point = new Vector3(i, 0, j);
+                if (!obsPos.Contains(point))
+                {
+                    GameObject tmp = Instantiate(tile, point, Quaternion.identity);
+                    tmp.transform.parent = container;
+                }
+                // yield return new WaitForSeconds(0.05f);
+            }
+        }
+    }
+}
